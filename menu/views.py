2a49798cf65baf361ce071e1c5bfbf53a8b139d8
Денис Рожков <dblnbls2013@gmail.com>
2022-12-18 10:58:40 +0300

    bugs fixed

--- menu/views.py
+++ menu/views.py
@@ -5,5 +5,5 @@ from .models import items, menuSections, design
 def show_menu(request):
     menu_positions = items.objects.all()
     sections = menuSections.objects.all()
-    designn  = design.objects.get(cafename='cafe')  
+    designn  = design.objects.all()[:1].get()  
     return render(request, 'menu.html',{'all_items':menu_positions, 'all_sections':sections, 'design':designn})
